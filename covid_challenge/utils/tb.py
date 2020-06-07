4689293001bb5cef28f06fe593f84c53cf0e697e
Eisen AI <none@nobody.com>
2020-06-07 12:38:55 +0200

    shoudl work

--- covid_challenge/utils/tb.py
+++ covid_challenge/utils/tb.py
@@ -1,6 +1,6 @@
 from flask import Flask
-
 from subprocess import Popen
+from multiprocessing import Process
 
 
 tb_cmd = [
@@ -16,9 +16,22 @@ tb_process = Popen(tb_cmd)
 
 app = Flask(__name__)
 
+server = Process(target=app.run, kwargs={'host': '0.0.0.0', 'port': '6006'})
+
+print(server)
+
 @app.route('/')
 def hello_world():
     tb_process.terminate()
-    exit(0)
 
-app.run(host='0.0.0.0', port='6006')
+    return 'Terminated'
+
+server.start()
+
+
+while True:
+    retcode = tb_process.poll()
+
+    if retcode is not None:
+        server.terminate()
+        exit(0)
