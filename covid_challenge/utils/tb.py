c5ec23e408ec0254efd945bed9520c83f594a424
Eisen AI <none@nobody.com>
2020-06-07 19:04:50 +0200

    strategy for TB comm

--- covid_challenge/utils/tb.py
+++ covid_challenge/utils/tb.py
@@ -1,3 +1,6 @@
+import requests
+import os
+
 from flask import Flask
 from subprocess import Popen
 from multiprocessing import Process
@@ -9,6 +12,19 @@ tb_cmd = [
     '--host=0.0.0.0',
     '--port=80'
 ]
+
+print('I will let the system know the IP for this tensorboard')
+
+resp = requests.get('http://169.254.169.254/latest/meta-data/public-ipv4')
+
+public_ip = resp.text
+
+job_id = os.listdir('/tmp/results')[0]
+
+r = requests.post(
+    'https://e2chj08pf8.execute-api.eu-central-1.amazonaws.com/v0/update-job-tb',
+    json={'job_id': job_id, 'public_ip': public_ip}
+)
 
 print('I am about to run Tensorboard via: {}'.format(tb_cmd))
 
@@ -27,7 +43,6 @@ def hello_world():
     return 'Terminated'
 
 server.start()
-
 
 while True:
     retcode = tb_process.poll()
