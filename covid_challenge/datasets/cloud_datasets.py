45c1703111889dd3ffb9b5e38b6f45f72523b1d0
Eisen AI <none@nobody.com>
2020-07-05 15:54:05 +0200

    added s3 json support

--- covid_challenge/datasets/cloud_datasets.py
+++ covid_challenge/datasets/cloud_datasets.py
@@ -2,7 +2,7 @@ import boto3
 import os
 import tempfile
 
-from eisen.datasets import MSDDataset
+from eisen.datasets import MSDDataset, JsonDataset
 from eisen.utils import read_json_from_file
 from covid_challenge import get_file_from_s3
 
@@ -38,3 +38,22 @@ class S3MSDDataset(MSDDataset):
         self.attributes = msd_dataset
 
         self.transform = transform
+
+
+class S3JsonDataset(JsonDataset):
+
+    def __init__(self, data_dir, json_file, aws_id=None, aws_secret=None, transform=None):
+        self.s3_client = boto3.client(
+            's3',
+            aws_access_key_id=aws_id,
+            aws_secret_access_key=aws_secret
+        )
+
+        self.tempdir = tempfile.mkdtemp()
+
+        json_file = get_file_from_s3(self.s3_client, os.path.join(data_dir, json_file), self.tempdir)
+
+        self.json_dataset = read_json_from_file(json_file)
+
+        self.transform = transform
+
